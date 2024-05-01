b7d4cb9c6bb1b7af78c7f28a4818baa6b0139e3d
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-05-01 21:34:23 +0300

    keys

--- react-FirstApp/src/components/App.jsx
+++ react-FirstApp/src/components/App.jsx
@@ -1,8 +1,17 @@
 import {Product} from './Product';
+import { Product } from "./Product";
+import { BookList } from "./BookList";
+const favouriteBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" },
+];
+>>>>>>> Stashed changes
 
 export default function App() {
   return (
     <div>
+<<<<<<< Updated upstream
       <h1>Products</h1>
 
       <Product
@@ -15,6 +24,9 @@ export default function App() {
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
       />
+      <h1>Books of the week</h1>
+      <BookList books={favouriteBooks} />
+>>>>>>> Stashed changes
     </div>
   );
 }
